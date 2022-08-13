7dad609d4e20127493df460e6c9c68994ede5e56
Fabiano Augusto de Mello <f.augmello@hotmail.com>
2022-08-13 11:58:40 -0300

    Add overview viewmodel

---
+++ tracker/tracker_presentation/src/main/java/app/fabianomello/tracker_presentation/overview/OverviewEvent.kt
@@ -0,0 +1,11 @@
+package app.fabianomello.tracker_presentation.overview
+
+import app.fabianomello.tracker_domain.model.TrackedFood
+
+sealed class OverviewEvent {
+    object OnNextDayClick: OverviewEvent()
+    object OnPreviousDayClick: OverviewEvent()
+    data class OnToggleMealClick(val meal: Meal): OverviewEvent()
+    data class OnDeleteTrackedFoodClick(val trackedFood: TrackedFood): OverviewEvent()
+    data class OnAddFoodClick(val meal: Meal): OverviewEvent()
+}
